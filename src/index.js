8ba39b7d8b0f2ec4e19c209e52eb6946e0f4d193
Amit Agarwal <amit@labnol.org>
2021-03-10 18:48:23 +0530

    VSCode Settings Updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
